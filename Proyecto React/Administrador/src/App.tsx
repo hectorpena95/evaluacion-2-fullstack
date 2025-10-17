7db6ea18fd2e7a58f28326d6f8998d9d37917aa5
Hector <hec.penac@duocuc.cl>
2025-10-17 16:06:27 -0300

    Paginas Log in y Sign in

--- Proyecto React/Administrador/src/App.tsx
+++ Proyecto React/Administrador/src/App.tsx
@@ -1,48 +1,47 @@
-// src/App.tsx
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'; 
+import { CarritoProvider } from './context/CarritoContext';
 
-// Importación del Contexto
-import { CarritoProvider } from './context/CarritoContext.jsx'; // <-- ¡IMPORTACIÓN DEL PROVIDER!
-
-// Importaciones de Componentes
+// Importaciones de Layout y Componentes Estructurales
 import Cabecera from './components/Cabecera';
 import PieDePagina from './components/PieDePagina';
 
-// Importaciones de Páginas
-import PaginaCatalogo from './paginas/PaginaCatalogo.jsx'; 
-import PaginaDetalleProducto from './paginas/PaginaDetalleProducto.jsx'; 
-import PaginaInicio from './paginas/PaginaInicio'; 
-import PaginaCarrito from './paginas/PaginaCarrito.jsx'; 
+// Importaciones de Páginas Existentes
+import PaginaInicio from './paginas/PaginaInicio';
+import PaginaCatalogo from './paginas/PaginaCatalogo';
+import PaginaDetalleProducto from './paginas/PaginaDetalleProducto';
+import PaginaCarrito from './paginas/PaginaCarrito';
+
+// NUEVAS IMPORTACIONES DE PÁGINAS DE AUTENTICACIÓN
+import PaginaLogin from './paginas/PaginaLogin'; 
+import PaginaRegistro from './paginas/PaginaRegistro'; 
+// ----------------------------------------------------
 
 function App() {
     return (
-        // *******************************************************
-        // 1. ENVOLVER TODA LA APLICACIÓN CON EL PROVIDER
         <CarritoProvider> 
             <Router>
                 <Cabecera />
                 
                 <Routes>
-                    
+                    {/* Rutas Principales */}
                     <Route path="/" element={<PaginaInicio />} />
                     <Route path="/catalogo" element={<PaginaCatalogo />} />
                     <Route path="/detalle-producto/:id" element={<PaginaDetalleProducto />} />
                     <Route path="/carrito" element={<PaginaCarrito />} /> 
                     
-                    <Route path="/login" element={<div>Página de Login</div>} />
-                    <Route path="/blog" element={<div>Página de Blog/Comunidad</div>} />
+                    {/* RUTAS DE AUTENTICACIÓN (NUEVAS) */}
+                    <Route path="/login" element={<PaginaLogin />} />
+                    <Route path="/registro" element={<PaginaRegistro />} />
                     
+                    {/* Rutas Adicionales y 404 */}
+                    <Route path="/blog" element={<div>Página de Blog/Comunidad</div>} />
                     <Route path="*" element={<div>404: Página No Encontrada</div>} />
-
                 </Routes>
 
                 <PieDePagina />
             </Router>
         </CarritoProvider> 
-        // *******************************************************
     );
 }
 
